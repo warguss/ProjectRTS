1b76e27434c5c96f286eba420cc6d3b8d365c7ac
root <root@localhost.localdomain>
2017-08-12 17:01:18 +0900

    Makefile , Test

--- ServerRoot/RTS_server/RTS_server.cpp
+++ ServerRoot/RTS_server/RTS_server.cpp
@@ -1,11 +1,13 @@
-// RTS_server.cpp : 콘솔 응용 프로그램에 대한 진입점을 정의합니다.
-//
+// 
 
-#include "stdafx.h"
+#include <unistd.h>
+#include <stdio.h>
 
 
-int _tmain(int argc, _TCHAR* argv[])
+int main(void)
 {
+	printf("Test\n");
+
 	return 0;
 }
 
