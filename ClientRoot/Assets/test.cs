a772d5550e9f9b51ccd9714dc3a6d5d3062a894c
blurtic <asdfgh25@naver.com>
2017-08-26 17:31:31 +0900

    통신 기본 테스트 완료.

--- ClientRoot/Assets/test.cs
+++ ClientRoot/Assets/test.cs
@@ -2,29 +2,64 @@
 using System.Collections;
 using Google.Protobuf;
 using System.IO;
+using System.Net.Sockets;
 
-public class test : MonoBehaviour {
-	// Use this for initialization
-	void Start () {
-        Google.Protobuf.Examples.AddressBook.Person asd = new Google.Protobuf.Examples.AddressBook.Person
-        {
-                Id = 3,
-                Name = "test"
-        };
+public class test : MonoBehaviour
+{
+    TcpClient client;
+    NetworkStream ns;
 
-        byte[] bmsg;
-        using (MemoryStream ms = new MemoryStream())
+    byte[] msgBuffer = new byte[8];
+
+    // Use this for initialization
+    void Start()
+    {
+        
+        client = new TcpClient("192.168.0.4", 10001);
+        ns = client.GetStream();
+
+    }
+	
+    // Update is called once per frame
+    void Update()
+    {
+        if (client != null)
         {
-            asd.WriteTo(ms);
-            bmsg = ms.ToArray();
+            if (ns.DataAvailable)
+            {
+                ns.BeginRead(msgBuffer, 0, msgBuffer.Length, DataReceive, ns);
+            }
+            else
+            {
+                
+            }
         }
 
-        var parsed = Google.Protobuf.Examples.AddressBook.Person.Parser.ParseFrom(bmsg);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Google.Protobuf.Examples.AddressBook.Person asd = new Google.Protobuf.Examples.AddressBook.Person
+            {
+                Id = 3,
+                Name = "test"
+            };
+
+            byte[] writeBuffer;
+            using (MemoryStream ms = new MemoryStream())
+            {
+                asd.WriteTo(ms);
+                writeBuffer = ms.ToArray();
+            }
+
+            Debug.Log("msg Length : " + writeBuffer.Length);
+            ns.Write(writeBuffer, 0, writeBuffer.Length);
+        }
+    }
+
+    void DataReceive(System.IAsyncResult ar)
+    {
+        var parsed = Google.Protobuf.Examples.AddressBook.Person.Parser.ParseFrom(msgBuffer);
         Debug.Log(parsed.Name);
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+        msgBuffer.Initialize();
+    }
 }
