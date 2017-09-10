6fd4dfc27257034849df6e85588a6bd0778c0237
warguss <kuc0104@naver.com>
2017-09-10 18:45:31 +0900

    Setting

--- ServerRoot/server2N/main/include/common/CUserPool.h
+++ ServerRoot/server2N/main/include/common/CUserPool.h
@@ -2,20 +2,21 @@
 #define _MODULE_UESRPOOL_H
 
 #include "CUser.h"
+#include <map>
 #define MAX_POOL 300
 
 
 class CUserPool
 {
 	private:
-		List<CUser> userPool;
+		map<int , CUser*> userInfo;
 
 	public:
 		CUserPool();
 		~CUserPool();
 
 		bool addUserInPool(int fd, int32_t x, int32_t y);
-		bool delUserInPool(CUser* user);
+		bool delUserInPool(int fd);
 
 		bool allSendEvent();
 };
