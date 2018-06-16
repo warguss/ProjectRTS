db785d59dfa55adb02b0dab9c9ca833221c5a10d
warguss <kuc0104@naver.com>
2018-06-16 16:42:56 +0900

    로그 클래스 추가

---
+++ ServerRoot/server2N/server/include/common/CLoging.cpp
@@ -0,0 +1,100 @@
+#include "CLoging.h"
+#include "CThreadLockManager.h"
+
+CLoging g_logger;
+
+CLoging::CLoging()
+{
+	_mutex = PTHREAD_MUTEX_INITIALIZER;
+	_cond = PTHREAD_COND_INITIALIZER;
+	_fp = NULL;
+	init();
+} 
+
+void CLoging::clear()
+{
+	/******************
+	 * AutoLock
+	 ******************/
+	CThreadLockManager autoLock(&_mutex, &_cond);
+	if ( _fp )
+	{
+		fclose(_fp);
+		_fp = NULL;
+	} 
+} 
+
+
+CLoging::~CLoging()
+{
+	clear();
+}
+
+void CLoging::init()
+{
+	/******************
+	 * AutoLock
+	 ******************/
+	CThreadLockManager autoLock(&_mutex, &_cond);
+	if ( !_fp )
+	{
+		_fp = fopen("./log/debug.log", "ar+");
+	}
+} 
+
+bool CLoging::wlog(int lv, const char* file, int line, const char* fmt, va_list args)
+{
+	/******************
+	 * AutoLock
+	 ******************/
+	CThreadLockManager autoLock(&_mutex, &_cond);
+
+	/******************
+	 * fp Set
+	 ******************/
+	FILE *fp = _fp;
+	char pMsg[8192];
+	va_list copied;
+	time_t cur;
+	time(&cur);
+
+	struct tm *t = localtime(&cur);
+	char pTime[64];
+	memset(pTime, '\0', sizeof(pTime));
+	sprintf(pTime, "%04d-%02d-%02d %02d:%02d:%02d", t->tm_year + 1900, t->tm_mon + 1, t->tm_mday, t->tm_hour, t->tm_min, t->tm_sec);
+
+	char pType[10];
+	memset(pType, '\0', sizeof(pType));
+	switch(lv)
+	{
+		case Trace:
+			strcpy(pType, "TRC");
+			break;
+		case Debug:
+			strcpy(pType, "DBG");
+			break;
+		case Info:
+			strcpy(pType, "INF");
+			break;
+		case Warn:
+			strcpy(pType, "WRN");
+			break;
+		case Error:
+			strcpy(pType, "ERR");
+			break;
+		case Fatal:
+			strcpy(pType, "FTL");
+			break;
+	}
+	va_copy(copied, args);
+	vsnprintf(pMsg, sizeof(pMsg), fmt, copied);
+	printf("print Test\n");
+	printf("%s|%s|%s(%d) :%s\n",  pType, pTime, file, line, pMsg);
+	if ( fp )
+	{
+		fprintf(fp, "%s|%s|%s(%d) :%s\n",  pType, pTime, file, line, pMsg);
+		fflush(fp);
+	}
+
+	va_end(copied);
+}
--- ServerRoot/server2N/server/include/common/CThreadLockManager.cpp
+++ ServerRoot/server2N/server/include/common/CThreadLockManager.cpp
@@ -11,6 +11,23 @@ pthread_cond_t *_listCond = NULL;
 
 extern int32_t g_sectorIdx;
 
+
+CThreadLockManager::CThreadLockManager(pthread_mutex_t* mutex, pthread_cond_t* cond)
+{
+	printf("Log Lock");
+	_sectorNo = -1;
+	_type = -1;
+
+	_mutex = mutex;
+	_cond = cond;
+	pthread_mutex_lock(_mutex);
+	while(!(cond))
+	{
+		pthread_cond_wait(_cond, _mutex);
+	}
+}
+
+
 CThreadLockManager::CThreadLockManager(int type, int sectorNo)
 {
 	pthread_mutex_t* mutex;
@@ -64,22 +81,30 @@ void CThreadLockManager::release()
 {
 	pthread_mutex_t* mutex;
 	pthread_cond_t* cond;
-	if ( _sectorNo < 0 )
+	if ( _sectorNo < 0 && _type > 0 )
 	{
+		printf("Test1\n");
 		mutex = (_type == READ_TYPE) ? &_rMutex : &_wMutex;
 		cond = (_type == READ_TYPE) ? &_rCond : &_wCond;
 
 		pthread_mutex_unlock(mutex);
 		pthread_cond_signal(cond);	
 	}
-	else
+	else if ( _sectorNo > 0 )
 	{
+		printf("Test2 (%d)\n", _sectorNo);
 		mutex = &_listMutex[_sectorNo];
 		cond = &_listCond[_sectorNo];
 
 		pthread_mutex_unlock(mutex);
 		pthread_cond_signal(cond);	
 	}
+	else
+	{
+		printf("Test3\n");
+		pthread_mutex_unlock(_mutex);
+		pthread_cond_signal(_cond);	
+	} 
 } 
 
 
