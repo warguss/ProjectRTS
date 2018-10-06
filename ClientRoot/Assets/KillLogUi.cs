4d9d2d5f493433cc3622b56b55b84d27be055f38
son-PC\son <sjh0696@gmail.com>
2018-10-06 16:22:23 +0900

    [warguss/ProjectRTS issue#84] [WIP] 킬로그 출력 - 킬로그 UI - 킬로그 텍스트 생성 로직 추가 - 킬로그 prefab 추가

---
+++ ClientRoot/Assets/KillLogUi.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KillLogUi : MonoBehaviour {
+
+    public static KillLogUi Instance;
+    public GameObject killLogTextPrefab; 
+    public Button testButton;
+    // Use this for initialization
+    void Start () {
+        Instance = this;
+        testButton.onClick.AddListener(OnClickTestButton);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void GenerateKillLog(string performerName, string victimName)
+    {
+        GameObject killLogObject = Instantiate(killLogTextPrefab, transform);
+        Text killLogText = killLogObject.GetComponent<Text>();
+        killLogText.text = string.Format("{0} kill {1}", performerName, victimName);
+        if(this.transform.childCount > 3)
+        {
+            GameObject removedObject = this.transform.GetChild(0).gameObject;
+            Destroy(removedObject);
+        }
+    }
+
+    void OnClickTestButton()
+    {
+        GenerateKillLog("test0", "test0");
+    }
+}
