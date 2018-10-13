675a293aad761e4ea2c06eb5d0ba0b5799974c0c
BLURTIC-LAPTOP\blurtic <asdfgh25@naver.com>
2018-10-13 18:46:22 +0900

    필드 아이템 작업중.

---
+++ ClientRoot/Assets/FieldItem.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ItemType : int
+{
+    Weapon = 0,
+    Recover = 1,
+}
+
+public class FieldItem : MonoBehaviour {
+
+    public SpriteRenderer spriteRenderer;
+
+    public ItemType ItemType { get; private set; }
+    public WeaponId WeaponId { get; private set; }
+    public int Amount { get; private set; }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Initialize(ItemType inItemType, WeaponId inWeaponId, int inAmount)
+    {
+        ItemType = inItemType;
+        WeaponId = inWeaponId;
+        Amount = inAmount;
+
+        RefreshImage();
+    }
+
+    void RefreshImage()
+    {
+        switch(ItemType)
+        {
+            case ItemType.Weapon:
+                {
+                    switch (WeaponId)
+                    {
+                        case WeaponId.Pistol:
+                            {
+                                spriteRenderer.color = Color.yellow;
+                                break;
+                            }
+                        case WeaponId.Sniper:
+                            {
+                                spriteRenderer.color = Color.cyan;
+                                break;
+                            }
+                    }
+                    break;
+                }
+            case ItemType.Recover:
+                {
+                    spriteRenderer.color = Color.green;
+                    break;
+                }
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            MainCharacter targetPlayer = other.gameObject.GetComponent<MainCharacter>();
+            Destroy(gameObject);
+        }
+    }
+}
