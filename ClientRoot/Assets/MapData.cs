d41c0fc94a858601ca7addc53d23a85591e62ce2
BLURTIC-LAPTOP\blurtic <asdfgh25@naver.com>
2019-01-20 18:19:13 +0900

    맵 데이터 파일에서 읽도록 수정.

--- ClientRoot/Assets/MapData.cs
+++ ClientRoot/Assets/MapData.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class TerrainInfo
@@ -23,18 +24,70 @@ public class MapData : MonoBehaviour {
 
     public void LoadMap()
     {
-        AddBox(new Vector2(0, 0), new Vector2(100, 1));
-        AddBox(new Vector2(99, 1), new Vector2(100, 100));
-        AddBox(new Vector2(0, 99), new Vector2(100, 100));
-        AddBox(new Vector2(0, 0), new Vector2(1, 100));
+        //AddBox(new Vector2(0, 0), new Vector2(100, 1));
+        //AddBox(new Vector2(99, 1), new Vector2(100, 100));
+        //AddBox(new Vector2(0, 99), new Vector2(100, 100));
+        //AddBox(new Vector2(0, 0), new Vector2(1, 100));
 
-        for(int i=0; i<20; i++)
+        //for(int i=0; i<20; i++)
+        //{
+        //    for(int j=0; j<25; j++)
+        //    {
+        //        AddBox(new Vector2(i * 5 + 4, j * 4 + 4), new Vector2(i * 5 + 4 + 3, j * 4 + 5));
+        //    }
+        //}
+
+        string path = "Assets/Resources/MapData";
+        string mapData;
+        string[] lines;
+        StreamReader reader = new StreamReader(path);
+        mapData = reader.ReadToEnd();
+        reader.Close();
+
+        lines = mapData.Split('\n');
+        for (int i = 0; i < lines.Length; i++)
         {
-            for(int j=0; j<25; j++)
+            bool isBox = false;
+            string[] points;
+            string lineData;
+            List<Vector2> pointVector = new List<Vector2>();
+
+            if (lines[i].StartsWith("b"))
             {
-                AddBox(new Vector2(i * 5 + 4, j * 4 + 4), new Vector2(i * 5 + 4 + 3, j * 4 + 5));
+                isBox = true;
+                lineData = lines[i].Substring(1);
             }
-        }
+            else
+                lineData = lines[i];
+
+            points = lineData.Split('/');
+            for(int j=0; j<points.Length; j++)
+            {
+                string[] coordinate;
+                coordinate = points[j].Split(',');
+
+                if(coordinate.Length != 2)
+                {
+                    //데이터 포맷 에러
+                }
+                Debug.Log(coordinate[0]);
+                Debug.Log(coordinate[1]);
+
+                float posX = float.Parse(coordinate[0]);
+                float posY = float.Parse(coordinate[1]);
+
+                pointVector.Add(new Vector2(posX, posY));
+            }
+
+            if (isBox)
+            {
+                AddBox(pointVector[0], pointVector[1]);
+            }
+            else
+            {
+                AddPolygon(pointVector.ToArray());
+            }
+        }    
     }
 
     public void DrawMap()
