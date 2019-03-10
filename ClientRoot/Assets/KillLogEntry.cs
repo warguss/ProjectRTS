66f3313fddc80dbc8d21170aa0a3e4791d5dae61
BLURTIC-LAPTOP\blurtic <asdfgh25@naver.com>
2019-03-10 16:36:26 +0900

    킬로그 적용.

--- ClientRoot/Assets/KillLogEntry.cs
+++ ClientRoot/Assets/KillLogEntry.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class KillLogObject : MonoBehaviour {
+public class KillLogEntry : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
--- ClientRoot/Assets/KillLogUi.cs
+++ ClientRoot/Assets/KillLogUi.cs
@@ -6,8 +6,10 @@ using UnityEngine.UI;
 public class KillLogUi : MonoBehaviour {
 
     public static KillLogUi Instance;
-    public GameObject killLogTextPrefab; 
+    public GameObject killLogTextPrefab;
+    public Transform KillLogPanel;
     public Button testButton;
+
     // Use this for initialization
     void Start () {
         Instance = this;
@@ -21,16 +23,16 @@ public class KillLogUi : MonoBehaviour {
 
     public void GenerateKillLog(string performerName, string victimName)
     {
-        GameObject killLogObject = Instantiate(killLogTextPrefab, transform);
+        GameObject killLogObject = Instantiate(killLogTextPrefab, KillLogPanel);
         string killLogString = string.Format("{0} kill {1}", performerName, victimName);
 
         Text killLogText = killLogObject.GetComponent<Text>();
         killLogText.text = killLogString;
         Debug.Log(killLogString);
 
-        if(this.transform.childCount > 3)
+        if(KillLogPanel.childCount > 5)
         {
-            GameObject removedObject = this.transform.GetChild(0).gameObject;
+            GameObject removedObject = this.KillLogPanel.GetChild(0).gameObject;
             Destroy(removedObject);
         }
     }
